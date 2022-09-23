d3e858adc4d6ad535344fa1c00c60105a0048a3a
GentlemadScientist <gentlemadscientist@gmail.com>
2022-09-23 22:49:43 +0900

    Rebuild with user json quatres

--- se_glyphs.service.worker.js
+++ se_glyphs.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1663940865|23661622856";
+const CACHE_VERSION = "1663940965|23761223305";
 const CACHE_PREFIX = "SE-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "se_glyphs.offline.html";
